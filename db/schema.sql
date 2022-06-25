9b39bf82857370d3b3ea8911364840a430a435e2
frsargua <frsargua@gmail.com>
2022-06-25 13:33:45 +0100

    database sql files

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS movie_db;
+CREATE DATABASE movie_db;
+
+USE movie_db;
+
+CREATE TABLE movie_names (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  movie_name VARCHAR(30) NOT NULL
+);S
+
+
+CREATE TABLE reviews (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  movie_name TEXT,
+  movie_id INT NOT NULL,
+  FOREIGN KEY (movie_id)
+  REFERENCES movie_names(id)
+  ON DELETE SET NULL 
+);
