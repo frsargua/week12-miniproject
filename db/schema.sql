0760bf1610ccfe01af9d882013a116dd96e2a401
frsargua <frsargua@gmail.com>
2022-06-25 18:29:29 +0100

    Removed extra character

--- db/schema.sql
+++ db/schema.sql
@@ -6,13 +6,13 @@ USE movie_db;
 CREATE TABLE movie_names (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   movie_name VARCHAR(30) NOT NULL
-);S
+);
 
 
 CREATE TABLE reviews (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   movie_name TEXT,
-  movie_id INT NOT NULL,
+  movie_id INT,
   FOREIGN KEY (movie_id)
   REFERENCES movie_names(id)
   ON DELETE SET NULL 
